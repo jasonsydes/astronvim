ef4d2c43a56bcaed8ef8e93b9a98d33ed5d4b285
Jason Sydes <sydes@uoregon.edu>
2025-01-30 15:44:21 -0800

    play nice with vscode

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- Play nicely with the vscode-neovim extension in VS Code
+  { import = "astrocommunity.recipes.vscode" },
   -- import/override with your plugins folder
 }
